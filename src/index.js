c45e2b4514aae3cc467ba442da82fc6caf787044
Mobolaji Johnson <abjmobolaji@gmail.com>
2021-10-22 11:05:39 +0100

    Initialised Dev Branch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser =require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }))
+const PORT = process.env.PORT||5000;
 
-const PORT = process.env.PORT||5000
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+
+
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT} \nhttp://localhost:${PORT}`))
