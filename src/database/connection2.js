f03075c3b630a1cd1b5e668c47935cedc56b4ea0
onyedikachii <onyedikachii@hardcorebiometric.com>
2021-11-01 16:24:01 +0100

    Added Endpoints for all cases and case status

--- src/database/connection2.js
+++ src/database/connection2.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 module.exports.databaseConnection = async() => {
     try {
         return mongoose.createConnection('mongodb://localhost:27017/usersDatabase')
-        console.log("Successfully established DB Connection")
     } catch (error) {
         console.log(error);
     }
